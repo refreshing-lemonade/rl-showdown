947cbd3dba611c9921522c82a73b8844621d12f9
RL <75630326+refreshing-lemonade@users.noreply.github.com>
2024-07-21 23:44:22 -0700

    Rename gen 2 ou copy

--- config/formats.ts
+++ config/formats.ts
@@ -168,7 +168,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		banlist: ['Uber', 'Smeargle + Ingrain', 'Sand Veil', 'Soundproof', 'Assist', 'Baton Pass + Block', 'Baton Pass + Mean Look', 'Baton Pass + Spider Web', 'Swagger'],
 	},
 	{
-		name: "[Gen 2] OU",
+		name: "[Gen 2] OU (Copy)",
 		mod: 'gen2',
 		ruleset: ['Standard'],
 		banlist: ['Uber', 'Mean Look + Baton Pass', 'Spider Web + Baton Pass'],
